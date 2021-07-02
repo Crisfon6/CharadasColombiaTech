32c6ab51efad1eb3af297ca48de2bd3ee80a4a8a
smercadog14 <smercadog14@gmail.com>
2021-07-02 10:59:30 -0500

    Add All

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { PersonComponent } from './person/person.component';
+import { AskComponent } from './ask/ask.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    PersonComponent,
+    AskComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
