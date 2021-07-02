015c26a15091d692a67503d9b0318c315ca811d7
iepenaranda <iepo960709@gmail.com>
2021-07-02 12:52:53 -0500

    Progress bar and question

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { AskComponent } from "./ask/ask.component";
+
+const routes: Routes = [
+  {
+    path: 'ask',
+    component: AskComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { PersonComponent } from './person/person.component';
 import { AskComponent } from './ask/ask.component';
 
+
 @NgModule({
   declarations: [
     AppComponent,
