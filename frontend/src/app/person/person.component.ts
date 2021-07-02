ec6f55be5553ab62dbd98a86f19e7b2b493e443e
smercadog14 <smercadog14@gmail.com>
2021-07-02 13:31:15 -0500

    Add DashBoard

--- frontend/src/app/person/person.component.ts
+++ frontend/src/app/person/person.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-person',
   templateUrl: './person.component.html',
-  styleUrls: ['./person.component.css']
+  styleUrls: ['./person.component.css'],
 })
 export class PersonComponent implements OnInit {
+  @Input() dataUser: any;
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  flip(event: any) {
+    // event.srcElement.classList.toggle('myflip');
+    // console.log(
+    //   'PersonComponent ~ event.srcElement.classList',
+    //   event.srcElement.classList
+    // );
+
+    event.path[4].classList.toggle('myflip');
+
+    console.log('PersonComponent ~ event', event);
   }
-
 }
