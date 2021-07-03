8c0fca3e6f48dc295c8341fd1092be2ad141a798
iepenaranda <iepo960709@gmail.com>
2021-07-02 20:05:36 -0500

    frontend

--- frontend/src/app/lobby/lobby.component.ts
+++ frontend/src/app/lobby/lobby.component.ts
@@ -8,41 +8,46 @@ import { SocketService } from '../services/socket-service.service';
 @Component({
   selector: 'app-lobby',
   templateUrl: './lobby.component.html',
-  styleUrls: ['./lobby.component.css']
+  styleUrls: ['./lobby.component.css'],
 })
 export class LobbyComponent implements OnInit {
-  roomId:string;
-  rooms:Observable<any[]>;
-  form:FormGroup;
-    constructor(private route:ActivatedRoute,private cookieService:CookieService,private fb:FormBuilder,private socketService:SocketService,private router:Router) { 
-      this.roomId ='';
-      this.rooms =this.socketService.rooms;
-  console.log('rooms',this.rooms);
-  
-  
-      this.form = this.fb.group({
-        name:['',Validators.required]
-      })
-    }
- 
-    ngOnInit(): void {
+  roomId: string;
+  rooms: Observable<any[]>;
+  form: FormGroup;
+  data: any;
+  constructor(
+    private route: ActivatedRoute,
+    private cookieService: CookieService,
+    private fb: FormBuilder,
+    private socketService: SocketService,
+    private router: Router
+  ) {
+    this.data = {};
+    this.roomId = '';
+    this.rooms = this.socketService.rooms;
+    console.log('rooms', this.rooms);
+
+    this.form = this.fb.group({
+      name: ['', Validators.required],
+    });
+  }
+
+  ngOnInit(): void {
     //  this.roomId = this.route.snapshot.params.id;
     //  this.socketService.rooms.subscribe(rooms=>{
     //    console.log('ROOMS',rooms);
     //   //  this.rooms =rooms;
     //  })
     // this.cookieService.set('room',this.room);
-     
-    }
-    go(roomId:any,username:any){
-      console.log('USERNAME',username.value);
-      console.log('ROOMID',roomId.id);
-      this.socketService.addPlayer(roomId.id,username.value)
-      this.router.navigateByUrl(`room/${roomId.id}/${username.value}`);
-      
-    }
-    send(){
-      this.socketService.createRoom(this.form.value.name);
-      console.log('rooms',this.rooms);
-    }
+  }
+  go(roomId: any, username: any) {
+    console.log('USERNAME', username.value);
+    console.log('ROOMID', roomId.id);
+    this.socketService.addPlayer(roomId.id, username.value);
+    this.router.navigateByUrl(`room/${roomId.id}/${username.value}`);
+  }
+  send() {
+    this.socketService.createRoom(this.form.value.name);
+    console.log('rooms', this.rooms);
+  }
 }
