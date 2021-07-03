daf24301fdb98460e7228ff8333eec5a1de3692d
smercadog14 <smercadog14@gmail.com>
2021-07-02 21:47:43 -0500

    add views

--- frontend/src/app/dashboard/dashboard.component.ts
+++ frontend/src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Subscription } from 'rxjs';
+import { elementAt } from 'rxjs/operators';
 import { profiles } from '../../assets/profiles';
 import { SocketService } from '../services/socket-service.service';
 
@@ -15,17 +16,20 @@ export class DashboardComponent implements OnInit {
   public win: boolean;
   public lose: boolean;
   public message: string;
-  idRoom:string;
-  room:any;
-  roomSub!:Subscription;
-  username:string;
-  game:any;
-  isLoading:boolean;
-  constructor(private route:ActivatedRoute,
-    private socketService:SocketService,private router:Router) {
-      this.isLoading=true;
-      this.idRoom = route.snapshot.params.id;
-      this.username = route.snapshot.params.player;
+  idRoom: string;
+  room: any;
+  roomSub!: Subscription;
+  username: string;
+  game: any;
+  isLoading: boolean;
+  constructor(
+    private route: ActivatedRoute,
+    private socketService: SocketService,
+    private router: Router
+  ) {
+    this.isLoading = true;
+    this.idRoom = route.snapshot.params.id;
+    this.username = route.snapshot.params.player;
     this.answer = '';
     this.win = false;
     this.lose = false;
@@ -34,25 +38,28 @@ export class DashboardComponent implements OnInit {
 
   ngOnInit(): void {
     console.log('profiles', profiles);
-    console.log('IDROOM',this.idRoom);
+    console.log('IDROOM', this.idRoom);
     this.socketWork();
   }
 
-  wrong(){
-    this.socketService.wrong(this.idRoom,this.username);
+  wrong() {
+    this.socketService.wrong(this.idRoom, this.username);
   }
 
-socketWork(){
-  this.game = this.socketService.getRoom(this.idRoom);
-  this.roomSub = this.socketService.currentRoom.subscribe(room=>{
-    console.log('subscribe');
-    console.log('ROOM',room);
-    this.room =room;
-    this.isLoading=false;
+  socketWork() {
+    this.game = this.socketService.getRoom(this.idRoom);
+    this.roomSub = this.socketService.currentRoom.subscribe((room) => {
+      console.log('subscribe');
+      console.log('ROOM', room);
+      this.room = room;
+      this.isLoading = false;
 
-    this.room = room});
-console.log('ROOMSUB',this.roomSub);
-}
+      this.room = room;
+
+      this.youWon(this.room.players);
+    });
+    console.log('ROOMSUB', this.roomSub);
+  }
   res(event: any) {
     console.log(event);
     if (this.answer === event) {
@@ -72,6 +79,15 @@ console.log('ROOMSUB',this.roomSub);
   e(event: any) {
     console.log(event);
     this.answer = event;
+  }
+  youWon(player: any) {
+    const playerFilt = player.filter((element: any) => element.active == true);
+
+    if (playerFilt.length == 1) {
+      if (playerFilt[0].player == this.username) {
+        alert('Ganaste!');
+      }
+    }
   }
 
   closeAlert() {
